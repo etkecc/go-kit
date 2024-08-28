984df56f2c21b6817b263510d5bee1af65e42584
Aine <aine@etke.cc>
2024-08-28 23:42:02 +0300

    add MatrixErrorFrom

--- errors.go
+++ errors.go
@@ -1,5 +1,10 @@
 package kit
 
+import (
+	"encoding/json"
+	"io"
+)
+
 // ErrorResponse represents an error response
 //
 //nolint:errname // ErrorResponse is a valid name
@@ -35,4 +40,14 @@ func (e MatrixError) Error() string {
 // NewMatrixError creates a new Matrix error
 func NewMatrixError(code, err string) MatrixError {
 	return MatrixError{Code: code, Err: err}
+}
+
+// MatrixErrorFrom creates a new Matrix error from io.Reader
+func MatrixErrorFrom(r io.Reader) MatrixError {
+	var matrixErr MatrixError
+	if err := json.NewDecoder(r).Decode(&matrixErr); err != nil {
+		return NewMatrixError("M_UNKNOWN", err.Error())
+	}
+
+	return matrixErr
 }
